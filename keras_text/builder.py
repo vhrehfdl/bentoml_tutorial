23e05b6f97e516bf5b72ec0f4ec7965a9c62d784
vhrehfdl <vhrehfdl7tp@naver.com>
2021-08-26 04:05:45 +0000

    update

--- keras_text/builder.py
+++ keras_text/builder.py
@@ -1,11 +1,8 @@
-# 1) import the custom BentoService defined above
 from service import KerasTextClassificationService
 from binary_pre_training import model, tokenizer
 
-# 2) `pack` it with required artifacts
 bento_svc = KerasTextClassificationService()
 bento_svc.pack('model', model)
 bento_svc.pack('tokenizer', tokenizer)
 
-# 3) save your BentoSerivce
 saved_path = bento_svc.save()
